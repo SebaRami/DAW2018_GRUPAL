98380bf97eb8b2eb14715460d76a0b28aec771ce
Sebastian Ramirez <sard1996@hotmail.com>
2018-06-25 11:47:44 -0500

    Index, presentacion mejorada

--- Proyecto_1P/js/scripts.js
+++ Proyecto_1P/js/scripts.js
@@ -1,6 +1,6 @@
 function busquedaFiltro(){
 	text = document.getElementById('texto').value;
-	var divs = document.querySelectorAll('div#images_search > div > img');
+	var divs = document.querySelectorAll('div#images_search');
 	for (div of divs){
 		var alt_img = div.alt;
 		if(alt_img.includes(text)){
