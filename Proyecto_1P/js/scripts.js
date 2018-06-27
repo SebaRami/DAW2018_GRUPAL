b3fe404e6a64ccc7ac88b57d946902e5fbf301e3
unknown <Alejandro_ramr777@hotmail.com>
2018-06-27 00:08:57 -0500

    searching of images with ajax by event on search button

--- Proyecto_1P/js/scripts.js
+++ Proyecto_1P/js/scripts.js
@@ -3,20 +3,43 @@ function buscarPersonaje(){
 	//get the text from the search input
 	searchInput = document.getElementById("buscar_personaje").value;
 	
-	//if there's no input value return
-	if(searchInput=="") return;
+	if(searchInput=="") {
+		$("div[class='images_found_section']").empty();
+		console.log("em");
+		return;
+	}
 	
-	//make ajax request to personajes.xml	
-	
-	$.get("data/personajes.xml",function(xml){
+	//clean the div of previous images to put the new ones, based on the new query
+	$("div[class='images_found_section']").empty();	
+			
+	//make ajax request to personajes.xml		
+	var resultsFound=false
+	$.get("data/personajes.xml",function(xml){		
 		$(xml).find('personaje').each(function(){
+			//get nombre
 			var nombre = $(this).find('nombre').text();
-			if(nombre.includes(searchInput)){
+			if(nombre.includes(searchInput) ){								
 				//from here take whatever you need and load it in the html
-				console.log(nombre);
-				//$("<li></li>").html(sTitle + ", " + sPublisher).appendTo("#dvContent ul");
+				
+				//the name in xml is the same as the .jpg file
+				var img_path="img/personajes/" + nombre + ".jpg";	
+				
+				//construct the hmtl img tag
+				var img_html="<img src='" + img_path + "' alt='" + nombre +"'>" ;
+				
+				//console.log(img_path);
+								
+				//load image
+				$("div[class='images_found_section']").append(img_html);
+				resultsFound=true;
 			}
-		});		
+		});
+		//if no result display meessage
+		if(resultsFound) {
+			$("div[class='images_found_section']").append("<p>Resultados que contienen : " + searchInput + "</p>")
+		}else{
+			$("div[class='images_found_section']").append("<p>No hay resultados para : " + searchInput + "</p>")		
+		};
 	});
 	
 	/*
@@ -35,6 +58,7 @@ function buscarPersonaje(){
 }
 
 
+//do the query on real time it gets very complex on enhancing user experience.
+//document.getElementById("buscar_personaje").addEventListener("keyup", buscarPersonaje);	
 
-document.getElementById("buscar_personaje").addEventListener("keyup", buscarPersonaje);	
 document.getElementById("search-button").addEventListener("click", buscarPersonaje);	
